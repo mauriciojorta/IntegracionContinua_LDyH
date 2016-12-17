2c67b3cae1fd90508f89882b64340f8ec8b043ac
Travis CI <travis@travis-ci.org>
2016-12-17 17:23:41 +0000

    Deploy code docs to GitHub Pages Travis build: 6
    Commit: 3ab4821baf6fe5f4d3f375e2206dffb175c836ff

---
+++ html/search/functions_65.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['exchange',['exchange',['../class_l_dy_h_1_1_integracion_continua_1_1_quicksort.html#a009e9122c41e6b4dbad0ee73a2a23a0b',1,'LDyH::IntegracionContinua::Quicksort']]]
+];
--- html/search/functions_71.js
+++ html/search/functions_71.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['quicksort',['Quicksort',['../class_l_dy_h_1_1_integracion_continua_1_1_quicksort.html',1,'LDyH::IntegracionContinua']]],
   ['quicksort',['quicksort',['../class_l_dy_h_1_1_integracion_continua_1_1_quicksort.html#a0f3c35b4a25bda865170445581c8a077',1,'LDyH::IntegracionContinua::Quicksort']]]
 ];
